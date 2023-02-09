d92c1eac21b3d9cbd951c69c23b17b4f119392aa
Alexandre-Lobo <aslobo@hotmail.com>
2023-02-08 21:22:20 -0300

    Padrão DTO para técnico

--- src/main/java/com/alexandre/helpdesk/resource/TecnicoResource.java
+++ src/main/java/com/alexandre/helpdesk/resource/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alexandre.helpdesk.domain.Tecnico;
+import com.alexandre.helpdesk.domain.dtos.TecnicoDto;
 import com.alexandre.helpdesk.service.TecnicoService;
 
 @RestController
@@ -18,10 +19,10 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDto> findById(@PathVariable Integer id){
 		Tecnico obj = service.findById(id);
 		
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDto(obj));
 		
 	}
 
