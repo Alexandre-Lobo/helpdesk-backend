ba7f090160abbee62491c70be453398ed72edb74
Alexandre-Lobo <aslobo@hotmail.com>
2023-02-16 21:27:56 -0300

    Endpoint findById para chamado

---
+++ src/main/java/com/alexandre/helpdesk/resource/ChamadoResource.java
@@ -0,0 +1,29 @@
+package com.alexandre.helpdesk.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alexandre.helpdesk.domain.Chamado;
+import com.alexandre.helpdesk.domain.dtos.ChamadoDto;
+import com.alexandre.helpdesk.service.ChamadoService;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+	
+	@Autowired
+	ChamadoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDto>findById(@PathVariable Integer id){
+		Chamado obj = service.findById(id);
+		return ResponseEntity.ok().body(new ChamadoDto(obj));
+	}
+	
+	
+
+}
